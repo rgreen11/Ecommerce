60b94bb3de54554fb055de8bc9206435ca4e8c3b
Richard Green <richardgreen@pursuit.org>
2019-08-09 08:23:32 -0400

    added images

---
+++ src/pages/homepage/Homepage.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../../sass/_main.scss';
+import Directory from '../../components/directory/directory.component';
+
+const Homepage = () => {
+    return (
+        <div className='homepage'>
+            <Directory />
+        </div>
+    )
+}
+
+export default Homepage
